7780bc590f76ad024e27618f0dd3ad1b57e829e7
IvanGerman <ivgvi@mail.ru>
2021-08-09 20:02:50 +0700

    feat: solved task 'Bubblesort Once00'

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-const getSquares = function (num) {
-  if (num === 1) {
-    alert("there are no consecutive perfect squares for number 1");
-    return;
-  };
-  if (num > 999999) {
-    alert(" the number you typed in is too big");
-    return;
-  };
-  if (num < 1) {
-    alert(" the number you typed in is too small");
-    return;
-  };
-  if (num % 2 === 0) {
-    alert(" no even numbers allowed");
-    return;
-  }
+// const getSquares = function (num) {
+//   if (num === 1) {
+//     alert("there are no consecutive perfect squares for number 1");
+//     return;
+//   };
+//   if (num > 999999) {
+//     alert(" the number you typed in is too big");
+//     return;
+//   };
+//   if (num < 1) {
+//     alert(" the number you typed in is too small");
+//     return;
+//   };
+//   if (num % 2 === 0) {
+//     alert(" no even numbers allowed");
+//     return;
+//   }
 
-  let secondNumber = ((num - 1) / 2) ** 2;
-  let firstNumber = secondNumber + num;
-  return (firstNumber + "-" + secondNumber);
+//   let secondNumber = ((num - 1) / 2) ** 2;
+//   let firstNumber = secondNumber + num;
+//   return (firstNumber + "-" + secondNumber);
+// };
+
+// getSquares(9);
+
+
+//---------------------------------------------------------------------------------------------
+
+//Bubblesort Once
+
+function bubblesortOnce(a) {
+
+  const clonedA = [...a];
+
+  for (let j = 0; j < clonedA.length; j++) {
+    if (clonedA[j] > clonedA[j + 1]) {
+      let x = clonedA[j];
+      clonedA[j] = clonedA[j + 1];
+      clonedA[j + 1] = x;
+    }
+  };
+
+  return clonedA;
 };
 
-getSquares(9);
+bubblesortOnce([54, 7, 5, 3, 1, 2, 4, 6, 3]);
